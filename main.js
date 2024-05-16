c9b9eabe172b7a0fe8103f72498cc6cbd34c715c
oozcode <antonio.lankin@gmail.com>
2024-05-16 10:58:36 -0400

    psuh

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 /*popup de servicios a agenda*/
 document.addEventListener("DOMContentLoaded", () => {
-  const abrirmodal = document.querySelector("#agendar");
   const modalElement = document.querySelector("#bootstrapModal");
+  const modalTitle = modalElement.querySelector(".modal-title");
+  const modalBody = modalElement.querySelector(".modal-body");
+
+  const cartas = document.querySelectorAll(".carta");
+
   const modal = new bootstrap.Modal(modalElement);
 
-  if (abrirmodal) {
-      abrirmodal.addEventListener("click", () => {
+  cartas.forEach(carta => {
+      carta.addEventListener("click", () => {
+          // Obtener el título y el texto personalizado de la carta clicada
+          const titulo = carta.querySelector("h2").innerText;
+          const mensaje = carta.getAttribute("data-mensaje");
+
+          // Actualizar el título y el texto del modal con los datos obtenidos
+          modalTitle.innerText = titulo;
+          modalBody.innerText = mensaje;
+
+          // Mostrar el modal
           modal.show();
       });
-  }
+  });
 });
