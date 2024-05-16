fbc60e49666dd3c180b833ace0147ff0fd77f332
oozcode <antonio.lankin@gmail.com>
2024-05-15 21:40:58 -0400

    popup servicios

---
+++ main.js
@@ -0,0 +1,12 @@
+const abrirmodal=
+document.querySelector("#mas");
+const btncerrarmodal=
+document.querySelector("#btn-cerrar");
+const modal=
+document.querySelector("#modal");
+const btnagenda=
+document.querySelector("#btn-agenda");
+
+abrirmodal.addEventListener("click",()=>{
+  modal.showModal()
+})
