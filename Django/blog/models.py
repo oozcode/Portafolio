6fbb8fcc46e3562ee9df42a07078bc9076a6e8fe
oozcode <antonio.lankin@gmail.com>
2024-06-26 19:40:56 -0400

    models

--- Django/blog/models.py
+++ Django/blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Servicio(models.Model):
+    nombre = models.CharField(max_length=50)
+    Imagen = models.ImageField
+    descripcion=models.CharField(max_length=50)
