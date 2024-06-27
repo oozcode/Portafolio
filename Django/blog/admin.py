852568b4fb6b05331ca2f2ab1425d576dfe9583d
oozcode <antonio.lankin@gmail.com>
2024-06-26 21:41:40 -0400

    Migracion completada y panel de admin

--- Django/blog/admin.py
+++ Django/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Servicio,agendar,especialista
 # Register your models here.
+
+admin.site.register(Servicio)
+admin.site.register(agendar)
+admin.site.register(especialista)
--- Django/blog/models.py
+++ Django/blog/models.py
@@ -1,21 +1,30 @@
 from django.db import models
-from django.contrib.auth.models import user
+#ooz adminadmin
 # Create your models here.
 class Servicio(models.Model):
     nombre = models.CharField(max_length=50)
     Imagen = models.ImageField()
-    descripcion=models.models.TextField()
+    descripcion=models.TextField()
+
+    def __str__(self):
+        return self.nombre
  
 class especialista (models.Model):
     nombre = models.CharField(max_length=50)
     Imagen = models.ImageField()
 
+    def __str__(self):
+        return self.nombre
+
 class agendar (models.Model):
     nombre = models.CharField(max_length=50)
-    telefono = models.IntegerField(max_length=30)
+    telefono = models.IntegerField()
     correo = models.EmailField (max_length=100,unique=True,blank=True,null=True)
     fecha = models.DateField()
     especialista = models.ForeignKey(especialista,on_delete=models.CASCADE)
     servicio = models.ForeignKey(Servicio,on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.nombre
 
 
