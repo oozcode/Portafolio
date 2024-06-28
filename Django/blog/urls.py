82b9d63784fbbbf1ec548eedeb84ed69d1781d65
Baisuke <bast.carcamo@duocuc.cl>
2024-06-27 22:38:14 -0400

    CRUD_SERVICIOS

--- Django/blog/urls.py
+++ Django/blog/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path("agenda/", views.agenda, name="agenda"),
     path("blog/", views.blog, name="blog"),
     path("servicios/", views.servicios, name="servicios"),
-    path("agregar-servicio/",views.agregar_servicio, name="agregar_servicio")
+    path("agregar-servicio/",views.agregar_servicio, name="agregar_servicio"),
+    path("listar-servicio/",views.listar_servicio, name="listar_servicio"),
+    path("modificar-servicio/<id>/",views.modificar_servicio, name="modificar_servicio"),
+    path("eliminar-servicio/<id>/",views.eliminar_servicio, name="eliminar_servicio"),
 ]
--- Django/blog/views.py
+++ Django/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect , get_object_or_404
 from .models import Servicio
 from .forms import ServicioForm
 
@@ -31,5 +31,42 @@ def agregar_servicio(request):
     data = {
         'form': ServicioForm()
     }
-    context = {}
-    return render(request, "pages/agregar.html",data)
+    if request.method == 'POST':
+        formulario = ServicioForm(data=request.POST, files= request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            data["mensaje"] = "Servicio agregado exitosamente"
+        else:
+            data["form"] = formulario
+    return render(request, "pages/agregar_servicio.html",data)
+
+def listar_servicio(request):
+    servicios = Servicio.objects.all()
+
+    data = {
+        'servicios': servicios
+    }
+    
+    return render(request,"pages/listar_servicio.html",data)
+
+def modificar_servicio(request, id):
+
+    servicios = get_object_or_404(Servicio, id=id)
+
+    data = {
+        'form': ServicioForm(instance=servicios)
+    }
+    if request.method == 'POST':
+        formulario = ServicioForm(data=request.POST, instance=servicios, files=request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect(to="listar_servicio")
+        else:
+            data["form"] = formulario
+
+    return render(request,"pages/modificar_servicio.html",data)
+
+def eliminar_servicio(request,id):
+    servicios = get_object_or_404(Servicio, id=id)
+    servicios.delete()
+    return redirect(to="listar_servicio")
